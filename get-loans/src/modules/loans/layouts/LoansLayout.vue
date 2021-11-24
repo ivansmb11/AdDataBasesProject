2c45210a67e3f3652248893da71cd1bff666877f
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-11-24 02:21:51 -0600

    Added my loans list

--- get-loans/src/modules/loans/layouts/LoansLayout.vue
+++ get-loans/src/modules/loans/layouts/LoansLayout.vue
@@ -32,7 +32,7 @@
 <section class="vh-100">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-md-8 col-lg-6 col-xl-8 offset-xl-1">
+      <div class="col-md-8 col-lg-6 col-xl-9 offset-xl-1">
         <router-view />
       </div>
     </div>
