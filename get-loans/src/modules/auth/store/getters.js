16c7889203c80f7897986281ef2aba329521a84d
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-11-24 18:22:51 -0600

    Added logout function

--- get-loans/src/modules/auth/store/getters.js
+++ get-loans/src/modules/auth/store/getters.js
@@ -2,3 +2,11 @@
 export const currentState = ( state ) => {
     return state.status
 }
+
+export const getUser = ( state ) => {
+    return state.user || ''
+}
+
+export const getFullName = ({ user }) => {
+    return `${ user?.name } ${ user?.lastName }` || ''
+}
