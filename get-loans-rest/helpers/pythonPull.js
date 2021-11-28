b70743670af352d09d1dc4881f4e698524e8487f
ivai <ismb9@hotmail.com>
2021-11-28 16:38:44 -0600

    Update pythonPull.js

--- get-loans-rest/helpers/pythonPull.js
+++ get-loans-rest/helpers/pythonPull.js
@@ -1,1 +1,6 @@
-// helper for Express execution on python functions that
+// helper for Express execution on python functions
+
+function pythonPull(amountRequested, income, co_applicant_income) {
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python',["AdDataBasesProject/Py/dataAnalyist.py", amountRequested, income, co_applicant_income]);
+ } 
