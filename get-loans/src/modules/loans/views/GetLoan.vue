d181c04e3e5f586f1813045457f1b62242a6b4f2
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-12-01 00:47:02 -0600

    Update loans components

--- get-loans/src/modules/loans/views/GetLoan.vue
+++ get-loans/src/modules/loans/views/GetLoan.vue
@@ -1,6 +1,16 @@
 <template>
-  <br><h1>Get Loan!</h1><br>
-  <GetLoanForm />
+  <div class="mt-4">
+    <h1>Get Loan!</h1>
+  </div>
+  <div class="container-fluid h-custom mt-4">
+    <div class="row d-flex justify-content-center h-100">
+      <div class="col-md-8 col-lg-8 col-xl-9 offset-xl-1">
+        <GetLoanForm />
+      </div>
+    </div>
+  </div>
+
+  
 </template>
 
 <script>
